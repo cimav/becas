e61f1b3c4a08a687d293a73ca08bdd1de0688cd8
Geovany Gameros <geovanygameros@gmail.com>
2018-01-30 15:16:49 -0700

    Barra de navegación con la pestaña actual activa

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,18 @@ class HomeController < ApplicationController
   before_action :auth_required
 
   def index
+    if session[:user_type].eql? 'User'
+      render template: 'home/user_index'
+    else
+      render template: 'home/staff_index'
+    end
+  end
+
+  def staff_index
+    @internships = Internship.where(staff_id:current_staff.id).where(status:Internship::ACTIVE)
+  end
+
+  def user_index
     @internships = Internship.all
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,15 @@
 module ApplicationHelper
   def active_class(link_path)
-    "active" if request.fullpath.start_with?(link_path)
+    "active" if request.path.start_with?(link_path)
   end
 
   def current_user
-    if session[:user_type].eql?'User'
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    else
-      @current_user ||= Staff.find(session[:user_id]) if session[:user_id]
-    end
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_staff
+    @current_user ||= Staff.find(session[:user_id]) if session[:user_id]
+  end
 
 
   def is_admin?
