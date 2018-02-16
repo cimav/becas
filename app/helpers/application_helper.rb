ac9e8f384ee1d780fea5e1b11c2c32a1ef3ef6e9
Geovany Gameros <geovanygameros@gmail.com>
2018-02-16 16:31:29 -0700

    Se implementa método para humanizar fecha y hora

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,30 @@
 module ApplicationHelper
 
-  def f_date(date)
+  def f_date_no_time(date)
     I18n.l(date, format: '%d de %B %Y').capitalize
+  end
+
+
+  def f_date(date)
+    if "#{date.year}#{date.month}#{date.day}" == "#{Time.now.year}#{Time.now.month}#{Time.now.day}"
+      r_date = "Hoy"
+      r_date = r_date + " a las " + date.strftime("%l:%M %P") if (date.hour != nil)
+    elsif "#{date.year}#{date.month}#{date.day}" == "#{Time.now.year}#{Time.now.month}#{Time.now.day - 1}"
+      r_date = "Ayer"
+      r_date = r_date + " a las " + date.strftime("%l:%M %P") if (date.hour != nil)
+    elsif "#{date.year}#{date.month}#{date.day}" == "#{Time.now.year}#{Time.now.month}#{Time.now.day + 1}"
+      r_date = "Mañana"
+      r_date = r_date + " a las " + date.strftime("%l:%M %P") if (date.hour != nil)
+    else
+      r_date = I18n.l(date, format: '%d de %B %Y a las %l:%M %P').capitalize
+      # d = "#{date.year}#{date.month}#{date.day}".to_i
+      # t = "#{Time.now.year}#{Time.now.month}#{Time.now.day}".to_i
+      # if d > t
+      #   r_date = "En #{t - d} días"
+      # else
+      #   r_date = "Hace #{d - t} días"
+      # end
+    end
   end
 
   def active_class(link_path)
