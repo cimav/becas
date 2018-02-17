b93d7d82bcb8ac1859998744abad0e52833daccb
Geovany Gameros <geovanygameros@gmail.com>
2018-02-17 12:27:02 -0700

    Se inicializa el estatus de los comentarios como activos

--- app/models/scholarship_comment.rb
+++ app/models/scholarship_comment.rb
@@ -1,4 +1,14 @@
 class ScholarshipComment < ApplicationRecord
   belongs_to :scholarship
   belongs_to :person, polymorphic: true
+
+  before_create :set_status
+
+  ACTIVE = 1
+  DELETED = 99
+
+  def set_status
+    self.status = ACTIVE
+  end
+
 end
