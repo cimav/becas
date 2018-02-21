fe2e482d2cc2c7efd2007a20542660a8ac6463d2
Geovany Gameros <geovanygameros@gmail.com>
2018-02-20 19:41:50 -0700

    Se incluye en script de arranque la ubicación del sapos

--- run-dev.sh
+++ run-dev.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 export GOOGLE_KEY='574213761508-kt23p4b77283mcf0jge8lgbso9u5k4c2.apps.googleusercontent.com'
 export GOOGLE_SECRET='mzWVoiZIpeWEWeQZ0dqgDZFK'
+export SAPOS_URL='http://localhost:3001'
+export SAPOS_DIR='/home/geovany/cimav/sapos'
 export RAILS_ENV='development'
 
 rails s -p 3000 -b 0.0.0.0
