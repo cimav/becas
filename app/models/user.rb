8cf2380838b642ae93873fe6d3af77a40c74bd9a
Geovany Gameros <geovanygameros@gmail.com>
2018-02-23 16:26:31 -0700

    Mejoras en pantallas de usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,23 @@
 class User < ApplicationRecord
   has_many :scholarship_comments, as: :person
+  before_create :set_status
 
   ADMIN = 1
   VIEWER = 2
   SUPER_USER = 1000
 
+
+  ACTIVE = 1
   DELETED = 99
 
   USER_TYPES = {ADMIN=>'Administrador', VIEWER=>'Espectador'}
+
+  def set_status
+    self.status = ACTIVE
+  end
+
+  def get_type
+    USER_TYPES[self.user_type]
+  end
 
 end
