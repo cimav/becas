a98ff8162d7083067d253dda38d4e260bafaadc1
Geovany Gameros <geovanygameros@gmail.com>
2018-03-21 12:49:48 -0700

    Se conecta con CEP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,27 +3,29 @@ class HomeController < ApplicationController
   def index
     if session[:user_type].eql? 'User'
       if is_admin?
-        @scholarships = Scholarship.where.not(status:Scholarship::DELETED).order("CASE
+        @scholarships = Scholarship.where.not(status: Scholarship::DELETED).order("CASE
                                   WHEN status = #{Scholarship::REQUESTED} THEN 1
-                                  WHEN status = #{Scholarship::APPROVED} THEN 2
-                                  WHEN status = #{Scholarship::ACTIVE} THEN 3
-                                  WHEN status = #{Scholarship::REJECTED} THEN 4
-                                  WHEN status = #{Scholarship::INACTIVE} THEN 5
+                                  WHEN status = #{Scholarship::TO_COMMITTEE} THEN 2
+                                  WHEN status = #{Scholarship::APPROVED} THEN 3
+                                  WHEN status = #{Scholarship::ACTIVE} THEN 4
+                                  WHEN status = #{Scholarship::REJECTED} THEN 5
+                                  WHEN status = #{Scholarship::INACTIVE} THEN 6
                                 END")
       elsif current_user.user_type.eql? User::DEPARTMENT_ASSISTANT
 
-        @scholarships = Scholarship.where.not(status:Scholarship::DELETED).where(person_type:'Internship').where(person_id:Internship.where(area:current_user.areas).pluck(:id)).order("CASE
+        @scholarships = Scholarship.where.not(status: Scholarship::DELETED).where(person_type: 'Internship').where(person_id: Internship.where(area: current_user.areas).pluck(:id)).order("CASE
                                 WHEN status = #{Scholarship::REQUESTED} THEN 1
-                                WHEN status = #{Scholarship::APPROVED} THEN 2
-                                WHEN status = #{Scholarship::ACTIVE} THEN 3
-                                WHEN status = #{Scholarship::REJECTED} THEN 4
-                                WHEN status = #{Scholarship::INACTIVE} THEN 5
+                                WHEN status = #{Scholarship::TO_COMMITTEE} THEN 2
+                                WHEN status = #{Scholarship::APPROVED} THEN 3
+                                WHEN status = #{Scholarship::ACTIVE} THEN 4
+                                WHEN status = #{Scholarship::REJECTED} THEN 5
+                                WHEN status = #{Scholarship::INACTIVE} THEN 6
                               END")
       end
       render template: 'home/user_index'
     else
       # se ordena por estatus de manera personalizada
-      @scholarships = Scholarship.where(person_type:'Internship').where.not(status:Scholarship::DELETED).where(person_id:(Internship.where(staff_id:current_staff.id).pluck(:id)))
+      @scholarships = Scholarship.where(person_type: 'Internship').where.not(status: Scholarship::DELETED).where(person_id: (Internship.where(staff_id: current_staff.id).pluck(:id)))
                           .order("CASE
                                 WHEN status = #{Scholarship::ACTIVE} THEN 1
                                 WHEN status = #{Scholarship::APPROVED} THEN 2
