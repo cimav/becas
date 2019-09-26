d162457b70cf2ea592b0f31c3b7e31faf6869a5c
Jonathan Hernández <jonathan.hernandez@gmail.com>
2019-09-26 12:36:28 -0600

    Cambiar nombre del estudiante

--- app/jobs/scholarship_notice_student_job.rb
+++ app/jobs/scholarship_notice_student_job.rb
@@ -3,8 +3,8 @@ class ScholarshipNoticeStudentJob < ApplicationJob
 
   def perform(scholarship_id, person_id, person_type)
     scholarship = Scholarship.find(scholarship_id)
-    #student = person_type.constantize.find(person_id)
-    student = User.first
+    student = person_type.constantize.find(person_id)
+    #student = User.first
 
     if !scholarship.scholarship_token
       scholarship.build_scholarship_token(token: (Digest::SHA256.hexdigest DateTime.now.to_s), status:ScholarshipToken::ACTIVE).save
